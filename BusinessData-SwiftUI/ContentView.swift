252589aac5cef1bbbb18e1594f7734081c8ed754
Muneer <NA>
2019-07-03 11:00:00 +0530

    Initial Commit

---
+++ BusinessData-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BusinessData-SwiftUI
+//
+//  Created by synup on 03/07/19.
+//  Copyright © 2019 synup. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
